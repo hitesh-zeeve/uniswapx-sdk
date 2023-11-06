764413d0eacc1e793eca8b536ead64a10645c138
Hitesh Sharma <hitesh.sharma@zeeve.io>
2023-11-07 01:49:42 +0530

    Local CDK network support
    Signed-off-by: Hitesh Sharma <hitesh.sharma@zeeve.io>

--- src/trade/DutchOrderTrade.ts
+++ src/trade/DutchOrderTrade.ts
@@ -1,4 +1,4 @@
-import { Currency, CurrencyAmount, Price, TradeType } from "@uniswap/sdk-core";
+import { Currency, CurrencyAmount, Price, TradeType } from "@hitesh.sharma_/sdk-core";
 
 import { DutchOrder, DutchOrderInfo } from "../order";
 
--- src/trade/utils.ts
+++ src/trade/utils.ts
@@ -1,4 +1,4 @@
-import { ChainId, Currency } from "@uniswap/sdk-core";
+import { ChainId, Currency } from "@hitesh.sharma_/sdk-core";
 import { constants } from "ethers";
 
 export enum NativeAssets {
